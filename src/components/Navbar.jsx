f0407652a206ad36bfe54fc2c01f9bc62cafbfcd
DJ Bates <blooviper@gmail.com>
2023-10-06 16:12:05 -0600

    changed title color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             {nav ? <AiOutlineClose className='hover:cursor-pointer' size={20}/> : <AiOutlineMenu className='hover:cursor-pointer' size={20} />}            
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] h-full ease-in-out duration-1000'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4 hover:cursor-pointer'>SQUASH.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#d9833b] m-4 hover:cursor-pointer'>SQUASH.</h1>
             <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-600 hover:cursor-pointer hover:text-[#d9833b]'>Home</li>
             <li className='p-4 border-b border-gray-600 hover:cursor-pointer hover:text-[#d9833b]'>Puppies</li>
